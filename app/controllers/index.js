d2e8480edfa242b17edc2508943e990f9f2968f6
Pablo Lluch <pablo.lluch@gmail.com>
2015-06-21 02:24:08 -0300

    More advanced test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,5 @@
 function doClick(e) {
-    alert($.label.text);
+	alert($.label.text);
 }
 
 
@@ -8,10 +8,23 @@ function doClick(e) {
 
 $.index.open();
 
+var counter = 0;
+
 var bluetooth = require('com.ewin.kanka.bluetooth');
+var interval = null;
+var temp;
+
 bluetooth.startScan(function(device) {
 	Ti.API.info("Connecting");
 	bluetooth.connectDevice(device.uniqueId, function(e) {
-		Ti.API.info(JSON.stringify(e));
+		temp = Math.round((e.temperature-32)/1.8);
+		if(!interval) {
+			interval = setInterval(function() {
+				$.label.setText(temp);
+				Ti.API.info(JSON.stringify(e));
+				$.webview.evalJS('addPoint("' + counter + '", ' + temp + ')');
+				counter+=5;
+			}, 5000);
+		}
 	});
 });
