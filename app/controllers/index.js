787f185a67ae2312b66b94a8300ded559268c1d1
Pablo Lluch <pablo.lluch@gmail.com>
2015-06-21 00:14:12 -0300

    Advance test

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,4 +2,16 @@ function doClick(e) {
     alert($.label.text);
 }
 
+
+
+
+
 $.index.open();
+
+var bluetooth = require('com.ewin.kanka.bluetooth');
+bluetooth.startScan(function(device) {
+	Ti.API.info("Connecting");
+	bluetooth.connectDevice(device.uniqueId, function(e) {
+		Ti.API.info(JSON.stringify(e));
+	});
+});
