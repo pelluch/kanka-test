c8e6726f3189bacce8bf93b3b91a05fde4efe626
Pablo Lluch <pablo.lluch@email.com>
2015-09-02 23:02:23 -0300

    Improve

--- app/controllers/device_row.js
+++ app/controllers/device_row.js
@@ -1,11 +1,27 @@
 
 var _device = arguments[0].device;
-var _graphic =  Alloy.createController('graphic');;
-var connected = false;
+var _graphic =  null;
 
 App.log('Row with device ', _device);
 
-$.uid.setText(_device.uniqueId);
+$.uid.setText(_device.deviceName);
+updateValues();
+
+
+function updateValues() {
+
+	App.log('Updating with values: ', {
+		temperature: _device.temperature,
+		lowThreshold: _device.lowThreshold,
+		highThreshold: _device.highThreshold,
+		preAlarmDelta: _device.preAlarmDelta
+	});
+	
+	$.temperatureLabel.setText(_device.temperature);
+	$.lowThresholdLabel.setText(_device.lowThreshold);
+	$.highThresholdLabel.setText(_device.highThreshold);
+	$.preAlarmDeltaLabel.setText(_device.preAlarmDelta);
+}
 
 exports.update = function(device) {
 	_device = device;
@@ -17,54 +33,60 @@ exports.update = function(device) {
 		$.button.hide();
 		$.status.setText("No visible");
 	}
-	if(device.temperature) {
-		$.temperature.setText(device.temperature);
-	}
 }
 
 var thresholded = false;
 function connect(e) {
-	if(!connected) {
-
-		params = {
-			onConnect: function(e) {
-				connected = true;
-				$.button.setTitle('Desconectar');
-				App.log('Connected');
-				$.status.setText("Conectado");
-			},
-			onDisconnect: function(e) {
-				$.button.setTitle('Conectar');
-				$.temperature.setText("N/A");
-				connected = false;
-				App.log('Disconnected');
-				$.status.setText("Desconectado");
-			},
-			onTemperatureChange: function(e) {
+	params = {
+		onConnect: function(e) {
+			App.log('onConnect');
+			$.button.setTitle('Desconectar');
+			$.status.setText("Conectado");
+			$.status.setColor('green');
+			_device = e;
+			updateValues();
+		},
+		onDisconnect: function(e) {
+			App.log('onDisconnect');
+			$.button.setTitle('Conectar');
+			$.status.setText("Desconectado");
+			_device = e;
+			updateValues();
+		},
+		onTemperatureChange: function(e) {
+			App.log('onTemperatureChange');
 				// App.log('Temperature changed');
 				// App.log(e);
 				var temp = e.temperature;
 
-				$.temperature.setText(temp + 'ºC');
+				// $.temperature.setText(temp + 'ºC');
 				if(_graphic) {
 					_graphic.onTemperatureChange(temp);
 				}
+				_device = e;
+				updateValues();
 			},
 			onThreshold: function(e) {
+				App.log('onThreshold');
 				if(thresholded) {
 					return;
 				}
 
 				thresholded = true;
-				App.log(e);
 
+				_device = e;
+				updateValues();
 
 			},
-			onArlarmAkcknowledge: function(e) {
-				App.log(e);
+			onAlarmAcknowledge: function(e) {
+				App.log('onAlarmAcknowledge');
+
+				_device = e;
+				updateValues();
 			},
 			onPrealarmStateChange: function(e) {
-				App.log(e);
+				App.log('onPrealarmStateChange');
+
 				if(e.state === "ACKNOWLEDGED_OR_REDUNDANT") {
 					if(OS_ANDROID) {
 						var notification = Titanium.Android.createNotification({
@@ -81,6 +103,8 @@ function connect(e) {
 						Titanium.Android.NotificationManager.notify(1, notification);
 					}
 				}
+				_device = e;
+				updateValues();
 			}
 		};
 
@@ -88,35 +112,65 @@ function connect(e) {
 		var highThreshold = parseInt($.highThreshold.getValue());
 		var alarmDelta = parseInt($.alarmDelta.getValue());
 
-		App.log(lowThreshold);
-		App.log(highThreshold);
-		App.log(alarmDelta);
+
 
 		if(!_.isNaN(lowThreshold)) {
-			App.log('App: Setting lowThreshold to ' + lowThreshold);
 			params.lowThreshold = lowThreshold;
 		}
 		if(!_.isNaN(highThreshold)) {
-			App.log('App: Setting highThreshold to ' + highThreshold);
 			params.highThreshold = highThreshold;
 		}
 		if(!_.isNaN(alarmDelta)) {
-			App.log('App: Setting alarmDelta to ' + alarmDelta);
 			params.preAlarmDelta = alarmDelta;
 		}
-
-		App.Bluetooth.connectDevice(_device.uniqueId, params);
-	} else {
-		App.Bluetooth.disconnectDevice(_device.uniqueId);
+		if(!_device.connected) {
+			App.Bluetooth.connectDevice(_device.uniqueId, params);
+		} else {
+			App.Bluetooth.disconnectDevice(_device.uniqueId, params);
+		}
 
 	}
-}
 
-function viewGraphic(e) {
-	if(OS_ANDROID) {
-		_graphic.getView().open();
-	} else {
-		Alloy.Globals.NavigationWindow.openWindow($.getView());
+	function viewGraphic(e) {
+		if(OS_ANDROID) {
+			_graphic.getView().open();
+		} else {
+			Alloy.Globals.NavigationWindow.openWindow($.getView());
+		}
+
 	}
 
-}
+	function resetDevice(e) {
+		App.Bluetooth.resetDevice(_device.uniqueId);
+	}
+
+	function acknowledgeAlarm(e) {
+		App.Bluetooth.acknowledgeAlarm(_device.uniqueId);
+	}
+
+
+	function highThresholdChanged(e) {
+		App.log('Changed');
+		if(!_.isNaN($.highThreshold.getValue()) && $.highThreshold.getValue() !== "") {
+			App.Bluetooth.setRecipe(_device.uniqueId, {
+				highThreshold: parseInt($.highThreshold.getValue())
+			});
+		}
+	}
+
+	function lowThresholdChanged(e) {
+		if(!_.isNaN($.lowThreshold.getValue()) && $.lowThreshold.getValue() !== "") {
+			App.Bluetooth.setRecipe(_device.uniqueId, {
+				lowThreshold: parseInt($.lowThreshold.getValue())
+			});
+		}
+	}
+
+	function preAlarmDeltaChanged(e) {
+		if(!_.isNaN($.alarmDelta.getValue()) && $.alarmDelta.getValue() !== "") {
+			App.Bluetooth.setRecipe(_device.uniqueId, {
+				preAlarmDelta: parseInt($.alarmDelta.getValue())
+			});
+		}
+	}
+
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,75 +5,32 @@ var rows = {};
 var counter = 0;
 
 
-if(OS_IOS) {
-	Alloy.Globals.NavigationWindow = $.getView();
-} else {
-	App.BluetoothEnable.enableBluetooth({
-		success: function(e) {
-			App.log('Success');
-		},
-		error: function(e) {
-			App.log('Error');
-		},
-		cancel: function(e) {
-			App.log('Cancel');
-		}
-	});
-}
-
 var interval = null;
 var temp;
 App.log('Test');
 
 function onOpen(e) {
-	if(OS_ANDROID) {
-		var activity = this.activity;
-		activity.addEventListener('create', function(e) {
-			App.log('Index create');
-		});
-		activity.addEventListener('start', function(e) {
-			App.log('Index start');
-		});
-		activity.addEventListener('resume', function(e) {
-			App.log('Index resume');
-		});
-		activity.addEventListener('pause', function(e) {
-			App.log('Index pause');
-		});
-		activity.addEventListener('stop', function(e) {
-			App.log('Index stop');
-		});
-		activity.addEventListener('destroy', function(e) {
-			App.log('Index destroy');
-		});
-		activity.addEventListener('restart', function(e) {
-			App.log('Index restart');
-		});
+	
+	App.Bluetooth.startScan({
+		onDiscover: function(device) {
 
-		App.Bluetooth.test = false;
-
-		App.Bluetooth.startScan({
-			onDiscover: function(device) {
-
-				App.log('Discovered ', device);
-				if(!rows[device.uniqueId]) {
-					var row = Alloy.createController('device_row', {
-						device: device
-					});
-					rows[device.uniqueId] = row;
-					$.scrollView.add(row.getView());
-				} else {
-					rows[device.uniqueId].update(device);
-				}
-
-			},
-			onUndiscover: function(device) {
-				App.log('Undiscovered ', device);
-				if(rows[device.uniqueId]) {
-					rows[device.uniqueId].update(device);
-				}
+			App.log('Discovered ', device);
+			if(!rows[device.uniqueId]) {
+				var row = Alloy.createController('device_row', {
+					device: device
+				});
+				rows[device.uniqueId] = row;
+				$.scrollView.add(row.getView());
+			} else {
+				rows[device.uniqueId].update(device);
 			}
-		});
 
-	}
+		},
+		onUndiscover: function(device) {
+			App.log('Undiscovered ', device);
+			if(rows[device.uniqueId]) {
+				rows[device.uniqueId].update(device);
+			}
+		}
+	});
 }
